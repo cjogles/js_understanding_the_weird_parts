649a190a68530e131afacb57600d713c634b5069
cjogles <jackson.ogles28@gmail.com>
2020-08-29 16:01:49 -0600

    working on section 3 still

--- 3_Types_Operators/SC/app.js
+++ 3_Types_Operators/SC/app.js
@@ -1,2 +1,10 @@
 // uncomment the code example you want to run.
 
+// ----------------------------------
+var a = 3 + 4;
+console.log(a);
+// ----------------------------------
+
+// syntax parser saw the plus sign, and added the two 
+// numbers. the plus sign is the add operator. 
+// its a function. This is "INFIX NOTATION".
