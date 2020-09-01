fdd98a8bc184a61505ea2e3a4d7b260fc0ca637c
cjogles <jackson.ogles28@gmail.com>
2020-09-01 15:16:54 -0600

    this keyword intro

--- 4_Objects_and_Functions/SC/app.js
+++ 4_Objects_and_Functions/SC/app.js
@@ -34,11 +34,58 @@
 
 // functions are objects
 
-function greet() {
-    console.log("hello");
-}
-console.log(greet)
-greet.language = "English";
-console.log(Object.keys(greet))
+// function greet() {
+//     console.log("hello");
+// }
+// console.log(greet)
+// greet.language = "English";
+// console.log(Object.keys(greet))
 
+// THIS keyword examples
+
+// ----------------------------------------
+function a() {         // function statement
+    console.log(this);
+}
+
+var b = function () { // function expression here
+    console.log(this); // because it results in value
+}
+
+a();
+b();
+
+var c = {
+    name: 'The c object',
+    log: function(){
+        var self = this; // set equal to by reference
+                        // same location in memory
+            // this is because its inside a method of 
+            // an object! Remember that. only one level 
+            // deep. 
+        self.name = 'Updated c object';
+        console.log(self);
+
+        var setname = function(newname){ // when its execution context was created, even though its sitting inside an object i created, its 'this' keyword will point to the global object!!! LAME. 
+            self.name = newname
+        }
+        setname("updated again! the c object") // updated global object :( 
+        console.log(self)
+    }
+}
+
+c.log();
+// ----------------------------------------
+
+// invoking a() will create an execution context with the 
+// keyword 'this'. What will 'this' point to? We have a global 
+// execution context, one with A() and one with B(). Each gets its 
+// own 'this' keyword. All point to the same address. The 
+// Global object the Window. So long story short,
+// when just invoking you will scope chain to the global 
+// object.
+// in the case of var c, when I invoke the method on an 
+// object, the this kewword becomes the object that that 
+// method is sitting inside of! We can update the 
+// this keyword then. 
 
